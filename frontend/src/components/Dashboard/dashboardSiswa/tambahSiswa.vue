ca1a4a048dfa550860c3e779ae9b0cec3b26909e
ArellSBiyantoro <arelsavero@gmail.com>
2025-07-08 09:03:01 +0700

    update: form user

--- frontend/src/components/Dashboard/dashboardSiswa/tambahSiswa.vue
+++ frontend/src/components/Dashboard/dashboardSiswa/tambahSiswa.vue
@@ -1,87 +1,15 @@
-<template>
-  <div class="form-container">
-    <div class="form-card">
-      <h1 class="headerb1">Tambah Akun Siswa</h1>
-      <n-divider class="divider" />
-      <n-form
-        ref="formRef"
-        class="form"
-        inline
-        :model="formValue"
-        :rules="rules"
-        :size="size"
-      >
-        <div class="grid-form">
-          <n-form-item label="Nama Lengkap" path="user.name" class="col-span-3">
-            <n-input
-              v-model:value="formValue.user.name"
-              placeholder="Tuliskan nama siswa disini"
-            />
-          </n-form-item>
-          <n-form-item label="Jenjang" path="user.jenjang" class="col-span-1">
-            <n-select
-              v-model:value="formValue.user.jenjang"
-              :options="optionsjenjang"
-              placeholder="Pilih jenjang pendidikan"
-            />
-          </n-form-item>
-        </div>
-        <div class="grid-form">
-          <n-form-item label="E-mail Siswa" path="user.email" class="col-span-4">
-            <n-input
-              v-model:value="formValue.user.email"
-              placeholder="Tuliskan email siswa disini"
-            />
-          </n-form-item>
-        </div>
-        <div class="grid-form">
-          <n-form-item label="Password Siswa" path="user.pass" class="col-span-4">
-            <n-input
-              v-model:value="formValue.user.pass"
-              placeholder="Tuliskan password siswa disini"
-              type="password"
-              show-password-on="mousedown"
-            />
-          </n-form-item>
-        </div>
-        <div class="grid-form">
-          <n-form-item label="No. WhatsApp Siswa" path="user.wa" class="col-span-2">
-           <n-input
-              v-model:value="formValue.user.wa"
-              placeholder="Tuliskan no. WhatsApp siswa disini"
-              type="tel"
-            />
-          </n-form-item> 
-          <n-form-item label="No. Telp Wali Siswa" path="user.wali" class="col-span-2">
-           <n-input
-              v-model:value="formValue.user.wali"
-              placeholder="Tuliskan no. telp wali siswa disini"
-              type="tel"
-            />
-          </n-form-item> 
-        </div>
-        <div class="grid-form">
-          <n-form-item label="Alamat Siswa" path="user.alamat" class="col-span-4">
-            <n-input
-              v-model:value="formValue.user.alamat"
-              placeholder="Tuliskan alamat siswa disini"
-            />
-          </n-form-item>
-        </div>
-      </n-form>
-
-      <butPrimerNormal
-        @click="handleValidateClick"
-        label="Simpan Akun Siswa"
-      />
-    </div>
-  </div>
-</template>
-
 <script setup>
 import { ref } from "vue";
 import { useMessage } from "naive-ui";
 import butPrimerNormal from "@/components/dirButton/butPrimerNormal.vue";
+import butSecondNormal from "@/components/dirButton/butSecondNormal.vue";
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+function handleBackClick() {
+  router.back()
+}
 
 const formRef = ref(null);
 const message = useMessage();
@@ -191,6 +119,91 @@ function handleValidateClick(e) {
 }
 </script>
 
+<template>
+  <div class="form-container">
+    <div class="form-card">
+      <h1 class="headerb1">Tambah Akun Siswa</h1>
+      <n-divider class="divider" />
+      <n-form
+        ref="formRef"
+        class="form"
+        inline
+        :model="formValue"
+        :rules="rules"
+        :size="size"
+      >
+        <div class="grid-form">
+          <n-form-item label="Nama Lengkap" path="user.name" class="col-span-3">
+            <n-input
+              v-model:value="formValue.user.name"
+              placeholder="Tuliskan nama siswa disini"
+            />
+          </n-form-item>
+          <n-form-item label="Jenjang" path="user.jenjang" class="col-span-1">
+            <n-select
+              v-model:value="formValue.user.jenjang"
+              :options="optionsjenjang"
+              placeholder="Pilih jenjang pendidikan"
+            />
+          </n-form-item>
+        </div>
+        <div class="grid-form">
+          <n-form-item label="E-mail Siswa" path="user.email" class="col-span-4">
+            <n-input
+              v-model:value="formValue.user.email"
+              placeholder="Tuliskan email siswa disini"
+            />
+          </n-form-item>
+        </div>
+        <div class="grid-form">
+          <n-form-item label="Password Siswa" path="user.pass" class="col-span-4">
+            <n-input
+              v-model:value="formValue.user.pass"
+              placeholder="Tuliskan password siswa disini"
+              type="password"
+              show-password-on="mousedown"
+            />
+          </n-form-item>
+        </div>
+        <div class="grid-form">
+          <n-form-item label="No. WhatsApp Siswa" path="user.wa" class="col-span-2">
+           <n-input
+              v-model:value="formValue.user.wa"
+              placeholder="Tuliskan no. WhatsApp siswa disini"
+              type="tel"
+            />
+          </n-form-item> 
+          <n-form-item label="No. Telp Wali Siswa" path="user.wali" class="col-span-2">
+           <n-input
+              v-model:value="formValue.user.wali"
+              placeholder="Tuliskan no. telp wali siswa disini"
+              type="tel"
+            />
+          </n-form-item> 
+        </div>
+        <div class="grid-form">
+          <n-form-item label="Alamat Siswa" path="user.alamat" class="col-span-4">
+            <n-input
+              v-model:value="formValue.user.alamat"
+              placeholder="Tuliskan alamat siswa disini"
+            />
+          </n-form-item>
+        </div>
+      </n-form>
+      <div class="button">
+        <butPrimerNormal
+          @click="handleValidateClick"
+          label="Simpan Akun Siswa"
+        />
+        <butSecondNormal
+          @click="handleBackClick"
+          label="Batal"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
 <style scoped>
 .form-container {
   width: 100%;
@@ -238,5 +251,10 @@ function handleValidateClick(e) {
 }
 ::v-deep(.n-base-selection) {
   border-radius: 8px !important;
+}
+.button{
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
 }
 </style>
