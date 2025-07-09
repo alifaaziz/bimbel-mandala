538508570e79395714a7cb0b841f9159eb06fe6c
alifaaziz <alifabdulaziz24@gmail.com>
2025-07-09 15:18:01 +0700

    feat: add ButImgTambahSecondNormal component and update handleTambahProgram method

--- frontend/src/components/Dashboard/dashboardProgram/daftarProgram.vue
+++ frontend/src/components/Dashboard/dashboardProgram/daftarProgram.vue
@@ -75,10 +75,11 @@
 <script>
 import { NIcon, NPagination } from 'naive-ui'
 import { CheckmarkCircleOutline, CloseCircleOutline } from '@vicons/ionicons5'
+import ButImgTambahSecondNormal from '@/components/dirButton/butImgTambahSecondNormal.vue'
 
 export default {
   name: 'DashboardContainer',
-  components: { NIcon, NPagination, CheckmarkCircleOutline, CloseCircleOutline },
+  components: { NIcon, NPagination, CheckmarkCircleOutline, CloseCircleOutline, ButImgTambahSecondNormal },
   data() {
     return {
       packageItems: [],
@@ -112,13 +113,14 @@ export default {
       alert(`Detail untuk ${item.name} (${item.slug})`);
     },
     handleTambahProgram() {
-      alert('Tambah Program');
+      this.$router.push('/dashboardadmin/programadmin/tambahprogram');
     }
   },
   mounted() {
     this.fetchClosestSchedules();
   }
 };
+</script>
 
 <style scoped>
 .dashboard-container {
